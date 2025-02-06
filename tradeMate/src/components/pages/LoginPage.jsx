a4de34a693cbd5220a0d8448f3a98c41e268af55
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2025-02-06 17:58:36 +0100

    feat : Add title and form

--- tradeMate/src/components/pages/LoginPage.jsx
+++ tradeMate/src/components/pages/LoginPage.jsx
@@ -1,3 +1,41 @@
+import {useState} from "react";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>
+  // state
+  const [inputEmail, setInputEmail] = useState('');
+
+  // comportement
+  const handleSubmit = (evt) => {
+    evt.preventDefault()
+    alert(`Bonjour ${inputEmail}`)
+    setInputEmail("")
+  }
+
+  const handleChange = (evt) => {
+    setInputEmail(evt.target.value)
+  }
+
+  // render
+  return <div>
+    <h1>Connexion</h1>
+
+    <form action="submit" onSubmit={handleSubmit}>
+
+      <input
+        value={inputEmail}
+        onChange={handleChange}
+        type="text"
+        placeholder="Email"
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        required
+      />
+
+      <button type="submit">Login</button>
+    </form>
+
+  </div>
 }
