952a3cac96a0b0084057260ebb828480d03a567e
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2025-02-21 16:07:13 +0100

    ✨ APP > install routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 import LoginPage from "./components/pages/login/LoginPage.jsx";
+import HomePage from "./components/pages/home/HomePage.jsx";
+import ErrorPage from "./components/pages/error/ErrorPage.jsx";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <LoginPage/>
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/home" element={<HomePage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
