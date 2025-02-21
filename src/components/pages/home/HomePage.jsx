c0a7d0fcd5df2c154268317ce99ac4423b3f2ff7
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2025-02-21 16:19:49 +0100

    ✨ LOGIN > Add redirection from/to HomePage

--- src/components/pages/home/HomePage.jsx
+++ src/components/pages/home/HomePage.jsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <Link to="/">
+        <button>Déconnexion</button>
+      </Link>
+    </div>
+  );
 }
--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,23 +1,23 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function LoginForm() {
   // state
-  const [inputEmail, setInputEmail] = useState('');
+  const [inputEmail, setInputEmail] = useState("");
 
   // comportement
   const handleSubmit = (evt) => {
-    evt.preventDefault()
-    alert(`Bonjour ${inputEmail}`)
-    setInputEmail("")
-  }
+    evt.preventDefault();
+    alert(`Bonjour ${inputEmail}`);
+    setInputEmail("");
+  };
 
   const handleChange = (evt) => {
-    setInputEmail(evt.target.value)
-  }
+    setInputEmail(evt.target.value);
+  };
 
   // render
   return (
-
     <form action="submit" onSubmit={handleSubmit}>
       <h1>Connexion</h1>
 
@@ -28,13 +28,10 @@ export default function LoginForm() {
         placeholder="Email"
       />
 
-      <input
-        type="password"
-        placeholder="Password"
-        required
-      />
+      <input type="password" placeholder="Password" required />
 
       <button type="submit">Login</button>
+      <Link to="/home">Vers HomePage</Link>
     </form>
-  )
+  );
 }
