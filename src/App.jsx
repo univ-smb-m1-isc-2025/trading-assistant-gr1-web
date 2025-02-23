ea685ed6e588cb7e750525eead2d07b380c638b0
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2025-02-23 12:00:35 +0100

    💄 LOGIN > Add styling add background

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import LoginPage from "./components/pages/login/LoginPage.jsx";
 import HomePage from "./components/pages/home/HomePage.jsx";
--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -17,10 +17,24 @@ export default function LoginPage() {
 }
 
 const LoginPageStyled = styled.div`
-  background: red;
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  ::before {
+    content: "";
+    background: url("/images/chart-background.jpg") rgba(0, 0, 0, 0.7);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: -1;
+  }
 `;
