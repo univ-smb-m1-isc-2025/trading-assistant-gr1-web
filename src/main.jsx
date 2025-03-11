cb2cf74c62e086b42d4aee5b538931a42eb67844
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2025-03-11 10:51:12 +0100

    ✅ update router

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+
+// Définition des routes avec le flag v7_startTransition
+const router = createBrowserRouter(
+  [
+    {
+      path: "/*",
+      element: <App />,
+    },
+  ],
+  {
+    future: {
+      v7_startTransition: true, // Active la fonctionnalité de React Router v7
+    },
+  }
+);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-        <App />
-      </GoogleOAuthProvider>
-    </BrowserRouter>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <RouterProvider router={router} />
+    </GoogleOAuthProvider>
   </StrictMode>
 );
