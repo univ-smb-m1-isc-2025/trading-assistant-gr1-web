cc98b7328059e2c17ce0ee751d4814c2a7d6eda4
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2025-03-24 12:04:15 +0100

    ✨ add allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: "all",
+    // allowedHosts: ["localhost", "www.trademate.oups.net"],
+  },
 });
