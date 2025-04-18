a80da113a423bd3dcf621988c5ff5b357635c869
mathisfeltrin <104620255+mathisfeltrin@users.noreply.github.com>
2025-04-18 15:28:58 +0200

    ♻️ refactor vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,12 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // Charger les variables d'environnement
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd()); // Charge les variables d'environnement
-  const apiURL = env.VITE_URL_API; // Accède à VITE_URL_API
-
+export default defineConfig(() => {
   return {
     plugins: [react()],
     server: {
       allowedHosts: ["localhost", "www.trademate.oups.net"],
-      // proxy: {
-      //   "/api": {
-      //     target: apiURL, // Utilisation de la variable d'environnement
-      //     changeOrigin: true,
-      //     rewrite: (path) => path.replace(/^\/api/, ""),
-      //   },
-      // },
     },
   };
 });
